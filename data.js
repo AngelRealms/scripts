68ed8a6e8c78a9dfba2c6ee5589237be07dddfbf
AngelRealms <ryanislucky11@gmail.com>
2018-03-23 14:23:52 -0400

    Updated data.js

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
-var all = document.getElementsByTagName("*")
-function func(element){
-	alert(element.tagName.toLowerCase() + "\n" + element.id + element.innerHTML);
-}
-for(var x=0; x < all.length; x++){
-	all[x].addEventListener('click',function(){func(this);},false);
+function init(){
+	var all = document.getElementsByTagName("*")
+	function func(element){
+		alert(element.tagName.toLowerCase() + "\n" + element.id + element.innerHTML);
+	}
+	for(var x=0; x < all.length; x++){
+		all[x].addEventListener('click',function(){func(this);},false);
+	}
 }
